add1c8ba3f94529ac118508620ff6f4fd9c9f1d4
Matt Duffin <matt@circlebuy.co>
2021-04-07 18:11:16 +0100

    Added adding and removing routers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listRouters } from './graphql/queries';
+import { createRouter as createRouterMutation, deleteRouter as deleteRouterMutation } from './graphql/mutations';
+
+const initialFormState = { name: '', address: '', status: 'COMMUNITY', proposer: '' };
 
 function App() {
+  const [routers, setRouters] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchRouters();
+  }, []);
+
+  async function fetchRouters() {
+    const apiData = await API.graphql({query: listRouters });
+    setRouters(apiData.data.listRouters.items);
+  }
+
+  async function createRouter() {
+    setFormData({ ...formData, 'proposer': "0x8C..."});
+    if (!formData.name || !formData.address || !formData.proposer) return;
+    await API.graphql({query: createRouterMutation, variables: {input: formData}});
+    setRouters([...routers, formData]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteRouter({ id }) {
+    const newRoutersArray = routers.filter(router => router.id !== id);
+    setRouters(newRoutersArray);
+    await API.graphql({ query: deleteRouterMutation, variables: { input: {id} }});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
+      <h1>My Routers App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Router name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'address': e.target.value})}
+        placeholder="Router address"
+        value={formData.address}
+      />
+      <button onClick={createRouter}>Create Router</button>
+      <div style={{marginBottom: 30}}>
+        {
+          routers.map(router => (
+            <div key={router.id || router.name}>
+              <h2>{router.name}</h2>
+              <p>{router.address}</p>
+              <button onClick={() => deleteRouter(router)}>Delete router</button>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
